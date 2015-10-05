3564b9ba291feb05a1be1d225d075464ee3628ba
mbark <barksten@kth.se>
2015-10-05 22:07:57 +0200

    Styling and such

--- harrypotter.js
+++ harrypotter.js
@@ -1,11 +1,11 @@
-var houses = ["Hufflepuff", "Ravenclaw", "Gryffindo", "Slytherin"];
+var houses = ["Hufflepuff", "Ravenclaw", "Gryffindor", "Slytherin"];
 
 function getHouse() {
 	var i = Math.floor(Math.random() * houses.length);
 	return houses[i];
 }
 
-var nextButton = document.getElementById('next-button');
+var nextButton = document.getElementById('sorting-hat');
 nextButton.addEventListener('click', function(event) {
-	document.getElementById('school').textContent = getHouse();
+	document.getElementById('school').textContent = getHouse() + "!";
 });
